3cf1e98f17a55a48411e753a79fa63ea2146feb5
Stéphan R <36258695+MAHEFA-Stephan@users.noreply.github.com>
2018-08-20 17:41:00 +0300

    resolve circularPrime problem

--- src/CircularPrimes.java
+++ src/CircularPrimes.java
@@ -1,5 +1,3 @@
-import java.math.BigDecimal;
-import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -12,25 +10,66 @@ import java.util.List;
  */
 public class CircularPrimes {
 
-	private static List<Integer> primes = new ArrayList<>();
-	private static List<Integer> circularPrimes = new ArrayList<>();
+	private static int[] primes = new int[(int) (Math.pow(10, 6) + 1)];
+	private static boolean[] tab = new boolean[primes.length];
+	private static List<Integer> circularPrimes = new ArrayList<Integer>();
 		
+	static boolean isCircular(char[] arr, int length, int shift) {
+		boolean skipRotation = (shift % length == 0) ? true : false;
+		boolean isCircular = false;
+		shift = (skipRotation) ? 1 : shift % length;
+
+		// When shift modulo by the length of the array is equal to 
+        // zero then no rotation is needed
+		for(int r = 0; r < length; r++) {
+			for(int i = 0; i < length; i++) {
+				// Avoid the last xor operation
+				if(i != length - 1) {
+					int a = Character.getNumericValue(arr[i]);
+					int b = Character.getNumericValue(arr[length - 1]);
+					
+					arr[i] = String.valueOf(b).toCharArray()[0];
+					arr[length - 1] = String.valueOf(a).toCharArray()[0];
+				}
+			}
+			
+			Integer rotateResult = Integer.parseInt(String.join(" ", Arrays.toString(arr).replace(",", "").replace("[", "").replace("]", "")).replace(" ", ""));
+						
+			if(rotateResult <= tab.length) {
+				if(tab[rotateResult] || rotateResult.toString().length() != length) {
+					isCircular = true;
+					break;
+				}
+				
+				isCircular = tab[rotateResult];
+			}
+		}
+		
+		return isCircular;
+	}
+	
 	static void loadPrimes() {
-		boolean[] tab = new boolean[(int) (Math.pow(10, 6) + 1)];
+		int index = 0;
 		
-		for(int i = 2; i < Math.sqrt(tab.length); i++) {
+		for(int i = 2; i < Math.sqrt(primes.length); i++) {
 			if(!tab[i]) {
-				for(int j = (i * i); j < tab.length; j += i) {
+				for(int j = (i * i); j < primes.length; j += i) {
 					tab[j] = true;
 				}
 			}
 		}
 		
-		for(int i = 2; i < tab.length; i++) {
-			if(!tab[i]) {
-				primes.add(i);
-			}
-		}
+		for(int f = 2; f < tab.length; f++) {        	
+        	if(!tab[f]) {
+        		primes[index++] = f;   
+        		
+        		char[] array = String.valueOf(f).toCharArray();
+				
+				if(!isCircular(array, array.length, array.length)) {
+					circularPrimes.add(f);
+				}
+        	}
+        }
 	}
 	
 	/**
@@ -41,7 +80,11 @@ public class CircularPrimes {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		long start = System.currentTimeMillis();
+		
 		loadPrimes();
-		System.out.println("");
+		
+		System.out.println(circularPrimes.size());
+		System.out.println("Solution took " + (System.currentTimeMillis() - start) + "ms");
 	}
 }
