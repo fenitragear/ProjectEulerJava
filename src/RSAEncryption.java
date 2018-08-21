9f89d9519730ce45e430543e47a408aa77e9292a
Stéphan R <36258695+MAHEFA-Stephan@users.noreply.github.com>
2018-08-21 15:57:07 +0300

    resolve RSAEncryption solution

--- src/RSAEncryption.java
+++ src/RSAEncryption.java
@@ -1,7 +1,6 @@
 
 /**
  * https://projecteuler.net/problem=182
- * TODO
  * 
  * @author Stéphan R.
  *
@@ -53,60 +52,53 @@ public class RSAEncryption {
 	static long sumOfAllValueE(long p, long q) {
 		long phi = (p - 1) * (q - 1);
 		long sumValueOfE = 0;
-		long u0 = 0;
-		long u1 = 1;
-		long u2 = 0;
-		long u3 = 0;
 		
-		for(int e = 1; e < (q/2); e++) {
-			if(!isCoprime(e, phi)) {
+		for(int e = 3; e < phi; e += 4) {
+			if(isCoprime(e, phi)) {
 				if((gcd((e -1), (p - 1)) + 1) * (gcd((e - 1), (q - 1)) + 1) == 9) {
-					u2 = e;
-					System.out.println("U2 = " + u2);
-					break;
+					sumValueOfE += e;
 				}
 			}
 		}
-		
-		for(int e = 1; e < (p/2); e++) {
-			if(!isCoprime(e, phi)) {
-				if((gcd((e -1), (p - 1)) + 1) * (gcd((e - 1), (q - 1)) + 1) == 9) {
-					u3 = e;
-					System.out.println("U3 = " + u3);
-					break;
-				}
-			}
-		}
-		
-		long u4 = (u2 + u3) % phi;
-		long u5 = phi - u4;
-		long u6 = phi - u3;
-		long u7 = phi - u2;
-		long u8 = phi - u1;
-		
-		System.out.println("U4 = U2 + U3 Mod N = " + u4);
-		System.out.println("U5 = N - U4 = " + u5);
-		System.out.println("U6 = N - U3 = " + u6);
-		System.out.println("U7 = N - U2 = " + u7);
-		System.out.println("U8 = N - U1 = " + u8);
-		
-		System.out.println("phi = " + phi);
-		
-		System.out.println("U1 + U8 = " + (u1 + u8));
-		System.out.println("U2 + U7 = " + (u2 + u7));
-		System.out.println("U3 + U6 = " + (u3 + u6));
-		System.out.println("U4 + U5 = " + (u4 + u5));
-		
+						
 		return sumValueOfE;
 	}
 
+	/**
+	 * The RSA encryption is based on the following procedure:
+	 * 
+	 * Generate two distinct primes p and q.
+	 * Compute n = pq and φ = (p-1)(q-1).
+	 * Find an integer e, 1 < e < φ, such that gcd(e,φ) = 1.
+	 * 
+	 * A message in this system is a number in the interval [0,n-1].
+	 * A text to be encrypted is then somehow converted to messages (numbers in the interval [0,n-1]).
+	 * To encrypt the text, for each message, m, c = m^e mod n is calculated.
+	 * 
+	 * To decrypt the text, the following procedure is needed: calculate d such that ed = 1 mod φ, then for each encrypted message, c, 
+	 * calculate m = c^d mod n.
+	 * There exist values of e and m such that me mod n = m.
+	 * We call messages m for which me mod n = m unconcealed messages.
+	 * 
+	 * An issue when choosing e is that there should not be too many unconcealed messages.
+	 * For instance, let p = 19 and q = 37.
+	 * Then n = 19 * 37 = 703 and φ = 18 * 36 = 648.
+	 * If we choose e = 181, then, although gcd(181,648) = 1 it turns out that all possible messages
+	 * m (0 ≤ m ≤ n-1) are unconcealed when calculating m^e mod n.
+	 * For any valid choice of e there exist some unconcealed messages.
+	 * It's important that the number of unconcealed messages is at a minimum.
+	 * 
+	 * Choose p = 1009 and q = 3643.
+	 * Find the sum of all values of e, 1 < e < φ(1009,3643) and gcd(e,φ) = 1, so that the number of unconcealed messages for this value of 
+	 * e is at a minimum
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {		
 		long start = System.currentTimeMillis();
 		
-		//438
-		System.out.println(sumOfAllValueE(11, 13));
-		// 399788195976
-		//System.out.println(sumOfAllValueE(1009, 3643));
+		System.out.println(sumOfAllValueE(1009, 3643));
+		
 		System.out.println("Solution took " + (System.currentTimeMillis() - start) + "ms");
 	}
 }
