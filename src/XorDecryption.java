e35554ba5b868e9d85dd15a252046737a4ddd43a
Stéphan R <36258695+MAHEFA-Stephan@users.noreply.github.com>
2018-09-19 10:58:52 +0300

    update #2
    algorithme optimisé

--- src/XorDecryption.java
+++ src/XorDecryption.java
@@ -1,11 +1,6 @@
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 /**
  * https://projecteuler.net/problem=59
@@ -46,27 +41,7 @@ public class XorDecryption {
 			14, 13, 19, 7, 2, 18, 3, 10, 2, 28, 23, 73, 79, 37, 9, 11, 68, 16, 10, 68, 15, 14, 18, 2, 79, 23, 2, 10, 10, 71, 7, 13, 20 , 79, 3, 11, 0, 22, 30, 67, 68, 19, 7, 1, 71,
 			8, 8, 8, 29, 29, 71, 0, 2, 71, 27, 12, 2, 79, 11, 9, 3, 29, 71, 60, 11, 9, 79, 11, 1, 79, 16, 15, 10, 68, 33, 14, 16, 15, 10, 22, 73
 	};
-	
-	private static Map<String, Double> letterFrequency = new HashMap<String, Double>() {{
-		put("a", 8.167); put("b", 1.492); put("c", 2.782); put("d", 4.253);
-		put("e", 12.702); put("f", 2.228); put("g", 2.015); put("h", 6.094);
-		put("i", 6.966); put("j", 0.153); put("k", 0.772); put("l", 4.025);
-		put("m", 2.406); put("n", 6.749); put("o", 7.507); put("p", 1.929);
-		put("q", 0.095); put("r", 5.987); put("s", 6.327); put("t", 9.056);		
-		put("u", 2.758); put("v", 0.978); put("w", 2.360); put("x", 0.150);
-		put("y", 1.974); put("z", 0.074);
-	}};
-	
-	private static Map<String, Double> firstLetterFrequency = new HashMap<String, Double>() {{
-		put("a", 11.682); put("b", 4.434); put("c", 5.238); put("d", 3.174);
-		put("e", 2.799); put("f", 4.027); put("g", 1.642); put("h", 4.200);
-		put("i", 7.294); put("j", 0.511); put("k", 0.456); put("l", 2.415);
-		put("m", 3.826); put("n", 2.284); put("o", 7.631); put("p", 4.319);
-		put("q", 0.222); put("r", 2.826); put("s", 6.686); put("t", 15.978);		
-		put("u", 1.183); put("v", 0.824); put("w", 5.497); put("x", 0.045);
-		put("y", 0.763); put("z", 0.045);
-	}};
-	
+		
 	private static List<Integer> acceptableValue = new ArrayList<Integer>() {{
 		// A-Z
 		add(65); add(66); add(67); add(68); add(69); add(70); add(71); add(72); add(73); add(74); add(75);
@@ -85,25 +60,23 @@ public class XorDecryption {
 		add(48); add(49); add(50); add(51); add(52); add(53); add(54); add(55); add(56); add(57);
 	}};
 	
+	/**
+	 * 
+	 */
 	static void analyze() {
-		int x = 0;
 		int sum = 0;
 		int temp = 0;
-		System.out.println("size = " + cipher.length);
 		byte[] keys = new byte[3];
 		byte[] cipherCopy = Arrays.copyOf(cipher, cipher.length);
 		boolean isNextFirstLetter = false;
 		boolean isProbableKey = true;
 		
 		for(int k = 0; k < keys.length; k++) {
-			for(int i = 0; i < letterFrequency.size(); i++) {
-				byte key = (i == 0 || isNextFirstLetter) ? ((String) firstLetterFrequency.keySet().toArray()[x++]).getBytes()[0] :
-					((String) letterFrequency.keySet().toArray()[i]).getBytes()[0];
-				
+			for(byte key = 97; key <= 122; key++) {				
 				for(int j = k; j < cipherCopy.length; j += 3) {
 					cipherCopy[j] ^= key;
 					
-					if((i ==  0 && cipherCopy[j] == 32) || !acceptableValue.contains((int) cipherCopy[j])) {
+					if((j ==  0 && cipherCopy[j] == 32) || !acceptableValue.contains((int) cipherCopy[j])) {
 						isProbableKey = false;
 						isNextFirstLetter = false;
 						cipherCopy = Arrays.copyOf(cipher, cipher.length);
@@ -111,9 +84,7 @@ public class XorDecryption {
 						break;
 					} else {
 						isProbableKey = true;
-						
-						if(!isNextFirstLetter)
-							isNextFirstLetter = cipherCopy[j] == 32;
+						isNextFirstLetter = (!isNextFirstLetter) ? cipherCopy[j] == 32 : isNextFirstLetter;
 						
 						temp += cipherCopy[j];
 					}
@@ -158,17 +129,7 @@ public class XorDecryption {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		long start = System.currentTimeMillis();
-		
-		letterFrequency = letterFrequency.entrySet().stream()
-                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
-                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
-                        (oldValue, newValue) -> oldValue, LinkedHashMap::new));
-		
-		firstLetterFrequency = firstLetterFrequency.entrySet().stream()
-                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
-                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
-                        (oldValue, newValue) -> oldValue, LinkedHashMap::new));						
+		long start = System.currentTimeMillis();				
 		
 		analyze();
 		
